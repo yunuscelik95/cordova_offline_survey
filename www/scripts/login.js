ed5e01ae8a053b08474d427e048d76deb1ac1ab2
yunuscelik95 <yunusclk95@gmail.com>
2026-03-02 23:07:22 +0300

    v2.10.1

--- www/scripts/login.js
+++ www/scripts/login.js
@@ -15,7 +15,7 @@ window.addEventListener('load', () => {
             uname: "",
             psw: "",
             oran: 0,
-            appVersion: "2.10.0",
+            appVersion: "2.10.1",
             // Güncelleme değişkenleri
             updateVisible: false,
             updateProgress: 0,
