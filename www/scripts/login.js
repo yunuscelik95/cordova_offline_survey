bd2bd04530e6581f6f446c6a4d176ac8cd91413e
yunuscelik95 <yunusclk95@gmail.com>
2026-03-06 21:16:38 +0300

    v2.10.17: BlokOzet cikis butonu eklendi

--- www/scripts/login.js
+++ www/scripts/login.js
@@ -15,7 +15,7 @@ window.addEventListener('load', () => {
             uname: "",
             psw: "",
             oran: 0,
-            appVersion: "2.10.16",
+            appVersion: "2.10.17",
             // Güncelleme değişkenleri
             updateVisible: false,
             updateProgress: 0,
