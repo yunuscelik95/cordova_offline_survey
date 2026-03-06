04a7842f259657248be846d4806bd3b348d9cbea
yunuscelik95 <yunusclk95@gmail.com>
2026-03-06 17:14:11 +0300

    v2.10.13 - Kiosk geri butonu, SES bilgi gosterimi, B12-B13 tek kisi atlama

--- www/scripts/login.js
+++ www/scripts/login.js
@@ -15,7 +15,7 @@ window.addEventListener('load', () => {
             uname: "",
             psw: "",
             oran: 0,
-            appVersion: "2.10.12",
+            appVersion: "2.10.13",
             // Güncelleme değişkenleri
             updateVisible: false,
             updateProgress: 0,
