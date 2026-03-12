f63b6b3966d0f15a3de89b3a36c2eb1b163f5dc8
yunuscelik95 <yunusclk95@gmail.com>
2026-03-12 15:35:15 +0300

    v2.11.7 - Durum kodlari sabit gomuldu

--- www/scripts/login.js
+++ www/scripts/login.js
@@ -15,7 +15,7 @@ window.addEventListener('load', () => {
             uname: "",
             psw: "",
             oran: 0,
-            appVersion: "2.11.6",
+            appVersion: "2.11.7",
             // Güncelleme değişkenleri
             updateVisible: false,
             updateProgress: 0,
